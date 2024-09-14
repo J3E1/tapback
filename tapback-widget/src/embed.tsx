4772bc2f76646eefd344defad8bc51fba6affc02
Jeel <baimsnik@gmail.com>
2024-09-14 22:11:25 +0530

    Updated widget pkg

--- tapback-widget/src/embed.tsx
+++ tapback-widget/src/embed.tsx
@@ -4,6 +4,11 @@ import './index.css';
 
 // Function to render the widget
 function renderFeedbackWidget() {
+	// Get the current script tag that loaded this widget
+	// const currentScript = document.currentScript as HTMLScriptElement;
+
+	// Extract data attributes (e.g., API key, other values)
+	// const apiKey = currentScript?.getAttribute('data-api-key');
 	// Create a container div and append it to the body
 	const widgetContainer = document.createElement('div');
 	widgetContainer.id = 'widget-container-root';
