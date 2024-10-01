4c93d8d14e4960a065b55396cc07dff98c61c3c0
Jeel <baimsnik@gmail.com>
2024-10-01 11:41:11 +0530

    Fixed auth actions bugs

--- tapback-dashboard/lib/auth.actions.ts
+++ tapback-dashboard/lib/auth.actions.ts
@@ -6,12 +6,8 @@ import { generateIdFromEntropySize } from 'lucia';
 import { cookies } from 'next/headers';
 import { argonConfig } from './constants';
 import prismaClient from './prisma-client';
-import {
-	loginSchema,
-	LoginSchema,
-	RegisterSchema,
-	registerSchema,
-} from './schemas';
+import { loginSchema, LoginSchema, RegisterSchema, registerSchema } from './schemas';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library';
 
 /**
  * Registers a new user in the database and creates a new session for it.
@@ -19,62 +15,73 @@ import {
  * @returns - An object with a success boolean and a message or error string.
  */
 export async function register(formData: RegisterSchema) {
-	const values = registerSchema.safeParse(formData);
-	if (values.success) {
-		const { name, email, password } = values.data;
+	try {
+		const values = registerSchema.safeParse(formData);
+		if (values.success) {
+			const { name, email, password } = values.data;
 
-		// Hash the user's password using Argon2.
-		const passwordHash = await hash(password, argonConfig);
+			// Hash the user's password using Argon2.
+			const passwordHash = await hash(password, argonConfig);
 
-		// Generate a unique identifier for the user.
-		const userId = generateIdFromEntropySize(10); // 16 characters long
+			// Generate a unique identifier for the user.
+			const userId = generateIdFromEntropySize(10); // 16 characters long
 
-		// Find basic plan id.
-		const basicPlan = await prismaClient.pricingPlan.findFirst({
-			where: {
-				name: 'BASIC',
-			},
-			select: {
-				id: true,
-			}
-		});
+			// Find basic plan id.
+			const basicPlan = await prismaClient.pricingPlan.findFirst({
+				where: {
+					name: 'BASIC',
+				},
+				select: {
+					id: true,
+				},
+			});
 
-		// Create a new user in the database.
-		const user = await prismaClient.user.create({
-			data: {
-				id: userId,
-				name: name,
-				email: email.toLowerCase(),
-				password: passwordHash,
-				pricingPlanId: basicPlan?.id,
-			},
-			include: {
-				pricingPlan: true,
-			}
-		});
+			// Create a new user in the database.
+			const user = await prismaClient.user.create({
+				data: {
+					id: userId,
+					name: name,
+					email: email.toLowerCase(),
+					password: passwordHash,
+					pricingPlanId: basicPlan?.id,
+				},
+				include: {
+					pricingPlan: true,
+				},
+			});
 
-		// Create a new session for the user.
-		const session = await lucia.createSession(userId, { });
+			// Create a new session for the user.
+			const session = await lucia.createSession(userId, { });
 
-		// Set the session cookie on the client.
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		cookies().set(
-			sessionCookie.name,
-			sessionCookie.value,
-			sessionCookie.attributes
-		);
+			// Set the session cookie on the client.
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 
-		// Return a success message to the client.
-		return {
-			message: 'Registered successfully',
-			success: true,
-		};
-	} else {
-		// Return an error message to the client.
-		return {
-			success: false,
-			error: values.error.message,
-		};
+			// Return a success message to the client.
+			return {
+				message: 'Registered successfully',
+				success: true,
+			};
+		} else {
+			// Return an error message to the client.
+			return {
+				success: false,
+				error: values.error.message,
+			};
+		}
+	} catch (error) {
+		if (error instanceof PrismaClientKnownRequestError) {
+			return {
+				success: false,
+				error: error.message,
+			};
+		} else {
+			console.log('🚀 ~ file: auth.actions.ts:82 ~ register ~ error:', error);
+			return {
+				success: false,
+				error: 'Something went wrong please try again latter',
+			};
+		}
 	}
 }
 
@@ -84,55 +91,62 @@ export async function register(formData: RegisterSchema) {
  * @returns - An object with a success boolean and a message or error string.
  */
 export async function login(formData: LoginSchema) {
-	const values = loginSchema.safeParse(formData);
-	if (values.success) {
-		const { email, password } = values.data;
+	try {
+		const values = loginSchema.safeParse(formData);
+		if (values.success) {
+			const { email, password } = values.data;
 
-		// Find the user in the database.
-		const existingUser = await prismaClient.user.findUnique({
-			where: { email: email.toLowerCase() },
-			include: {
-				pricingPlan: true,
+			// Find the user in the database.
+			const existingUser = await prismaClient.user.findUnique({
+				where: { email: email.toLowerCase() },
+				include: {
+					pricingPlan: true,
+				},
+			});
+
+			// If the user doesn't exist, return an error.
+			if (!existingUser) {
+				return { success: false, error: 'Incorrect email or password' };
 			}
-		});
 
-		// If the user doesn't exist, return an error.
-		if (!existingUser) {
-			return { success: false, error: 'Incorrect email or password' };
+			// Verify the user's password using Argon2.
+			const validPassword = await verify(existingUser.password, password, argonConfig);
+
+			// If the password is invalid, return an error.
+			if (!validPassword) {
+				return { success: false, error: 'Incorrect email or password' };
+			}
+
+			// Create a new session for the user.
+			const session = await lucia.createSession(existingUser.id, {});
+
+			// Set the session cookie on the client.
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+			return {
+				message: 'Logged in successfully',
+				success: true,
+			};
+		} else {
+			// Return an error message to the client.
+			return {
+				success: false,
+				error: values.error.message,
+			};
 		}
-
-		// Verify the user's password using Argon2.
-		const validPassword = await verify(
-			existingUser.password,
-			password,
-			argonConfig
-		);
-
-		// If the password is invalid, return an error.
-		if (!validPassword) {
-			return { success: false, error: 'Incorrect email or password' };
+	} catch(error) {
+		if (error instanceof PrismaClientKnownRequestError) {
+			return {
+				success: false,
+				error: error.message,
+			};
+		} else {
+			console.log('🚀 ~ file: auth.actions.ts:login ~ error:', error);
+			return {
+				success: false,
+				error: 'Something went wrong please try again latter',
+			};
 		}
-
-		// Create a new session for the user.
-		const session = await lucia.createSession(existingUser.id,  { });
-
-		// Set the session cookie on the client.
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		cookies().set(
-			sessionCookie.name,
-			sessionCookie.value,
-			sessionCookie.attributes
-		);
-		return {
-			message: 'Logged in successfully',
-			success: true,
-		};
-	} else {
-		// Return an error message to the client.
-		return {
-			success: false,
-			error: values.error.message,
-		};
 	}
 }
 
@@ -152,11 +166,7 @@ export async function logout() {
 	await lucia.invalidateSession(session.id);
 
 	const sessionCookie = lucia.createBlankSessionCookie();
-	cookies().set(
-		sessionCookie.name,
-		sessionCookie.value,
-		sessionCookie.attributes
-	);
+	cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 	return {
 		success: true,
 		message: 'Logged out successfully',
